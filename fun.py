aa62c559838bdfb54253a7970ce74e2b74e76d8a
aleghyk <aleghyk@gmail.com>
2018-11-21 11:02:09 +0200

    Merge pull request #1 from aleghyk/lmm01
    first modifying
    
    Signed-off-by: aleghyk <aleghyk@gmail.com>

--- fun.py
+++ fun.py
@@ -1,6 +1,30 @@
+"""
+lib modules
+
+assertion = your own exception assertion example:
+
+def some_func (some_var):
+    assert some_var !=''
+    some_other_func()
+    some_other_var = "some_string" """
+
+
 def starter (p):
-    print (str(p) + "test")
+    if p == '':
+        print ("incorect function call /n")
+        return 1
+        print (str(p) + "test")
     return input("some text")
 def stopper ():
     print ("stop this")
     return
+def dict_open(file, person):
+    if not file or not person:
+        print("somethig went wrong")
+    elif not person[2]:
+        print("something wrong with a person")
+    else:
+        print("ok")
+    dict_file = open(file)
+    print(dict_file.read())
+
--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,10 @@
 import math
 import random
-import fun.py
+import fun
 
 random_1 = random.randint (1,100)
 print(math.sqrt(random_1))
+fun.stopper()
+pers = ["oleh", "alex", "alex"]
+#fun.dict_open("t1.py", pers)
+fun.dict_open("t1.py","q")
