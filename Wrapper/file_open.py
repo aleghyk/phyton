3e2b026bb34756ef3043803621d00ff6b7a92dcc
aleghyk <aleghyk@gmail.com>
2018-11-23 11:36:53 +0200

    file contains ammending
    Signed-off-by: aleghyk <aleghyk@gmail.com>

--- Wrapper/file_open.py
+++ Wrapper/file_open.py
@@ -19,13 +19,3 @@ def Config_Open(cfg_f_n):
         print ('\n'+cfg)
 
     ConfigFile.close
-def list_modif ():
-    list1 = ['asd', 2, '1', 'country']
-    list2 = [2,5,6,9]
-    print (str(list1) + "   &    " + str(list2))
-    list1.append(list2 [2])
-    list2.insert( int(list1[2]), str(list2[3]))
-    print (str (list1) + '    &    ' + str (list2) )
-
-
-list_modif()
--- Wrapper/modules.py
+++ Wrapper/modules.py
@@ -1,8 +1,18 @@
-theList = ["a","b","c"]
-print (theList)
-joinedString = ",".join(theList)
-targetList = joinedString
-print(targetList)
-targetList = filter(lambda x: len(x)>0, targetList)
+def list_test():
+    theList = ["a","b","c"]
+    print (theList)
+    joinedString = ",".join(theList)
+    targetList = joinedString
+    print(targetList)
+    targetList = filter(lambda x: len(x)>0, targetList)
+    print (targetList)
 
-print (targetList)
+def list_modif ():
+    list1 = ['asd', 2, '1', 'country']
+    list2 = [2,5,6,9]
+    print (str(list1) + "   &    " + str(list2))
+    list1.append(list2 [2])
+    list2.insert( int(list1[2]), str(list2[3]))
+    print (str (list1) + '    &    ' + str (list2) )
+
+list_modif()
